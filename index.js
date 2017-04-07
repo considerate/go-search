7f08e91cb04bb1b4f3ef386fd5fd48da826faff9
Viktor Kronvall <viktor.kronvall@gmail.com>
2017-04-07 16:08:40 +0200

    GUI with query into mocked data

--- index.js
+++ index.js
@@ -3,10 +3,35 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Set up view rendering using handlebars templates
+app.set('view engine', 'html');
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views')
+
+const context = {
+    results: [
+    {
+        "name": "read",
+        "arguments": [["x", "int"], ["y", "double"], ["str", "string"]],
+        "returns": "string"
+    },
+    {
+        "name": "write",
+        "arguments": [["offset", "int"], ["length", "double"]],
+        "returns": ""
+    },
+    ],
+};
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+    const query = req.query.q;
+    //TODO: replace mocked context with results from elasticsearch
+    const results = context.results
+        .filter(r => (r.name || '').indexOf(query) != -1);
+    res.render('index', {
+        results,
+    });
 });
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
-})
+    console.log('Example app listening on port ' + PORT)
+});
