95398775590e7d05ad04b8789ba73add6432bd26
Max Landauer <max.landauer@live.at>
2017-04-10 19:46:59 +0200

    Add multi-line function declaration support, add tests.go file with some generic function declarations for better test coverage

---
+++ files/tests.go
@@ -0,0 +1,20 @@
+func funcName() {
+    func funcName() {
+func funcName()
+{
+func funcName() { funcBody }
+func funcName() int {
+func funcName()
+    int { funcBody { block } }
+func funcName(param1 type1) {
+func funcName(param1 type1, param2 type2) {
+func funcName(param1, param2 type) {
+func funcName(type1, type2) {
+func funcName() []returnType {
+func funcName(param1 *type1) {
+func funcName(param1 []type1) {
+func funcName(param1 []*type1) {
+func (rec1 recType1) funcName() {
+func (rec1 recType1, rec2 recType2) funcName() {
+func (param1 type1, param2 type2) int {
+func(param1 type1) {
