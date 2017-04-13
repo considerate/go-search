83dba23dde24c7dd2acd243502644fc79e957aca
Max Landauer <max.landauer@live.at>
2017-04-13 12:41:44 +0200

    inMatch was set to true at wrong place.

--- files/tests.go
+++ files/tests.go
@@ -3,9 +3,9 @@ func funcName() {
 func funcName()
 {
 func funcName() { funcBody }
-func funcName() int {
+func funcName() returnType {
 func funcName()
-    int { funcBody { block } }
+    returnType { funcBody { block } }
 func funcName(param1 type1) {
 func funcName(param1 type1, param2 type2) {
 func funcName(param1, param2 type) {
@@ -16,5 +16,10 @@ func funcName(param1 []type1) {
 func funcName(param1 []*type1) {
 func (rec1 recType1) funcName() {
 func (rec1 recType1, rec2 recType2) funcName() {
-func (param1 type1, param2 type2) int {
+func (param1 type1) returnType {
+func (param1 type1) (returnType) {
+func (param1 type1) (returnParam returnType) {
+func (param1 type1) (returnParam1 returnType1, returnParam2 returnType2) {
 func(param1 type1) {
+func (param1 ...type1) {
+func (param1 ...interface{}) {
