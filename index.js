4a130d11d44a7a8b307cc9af0c18784ab3976660
Max Landauer <max.landauer@live.at>
2017-05-05 19:18:00 +0200

    Build query from user input

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const {tokenizeString} = require('./Tokenizer.js');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
 
 // Set up view rendering using handlebars templates
 app.set('view engine', 'html');
@@ -25,6 +32,102 @@ const context = {
 app.get('/', function(req, res) {
     const query = req.query.q;
     //TODO: replace mocked context with results from elasticsearch
+    tokenizeString(query).then(function(result) {
+        var func = JSON.stringify(result)
+        var json = JSON.parse(func)
+
+        var scriptString = "int nonmatch = 0;" +
+            "int additional = (int) doc['parameters_info.__sz'].value;"
+        for (var key in json[0].object_info) {
+            if (json[0].object_info.hasOwnProperty(key) && key != '__sz') {
+                scriptString +=
+                    "try {" +
+                    "   nonmatch += (int) Math.abs(doc['parameters_info." + key + "'][0] - " + json[0].object_info[key] + ");" +
+                    "   additional -= doc['parameters_info." + key + "'][0];" +
+                    "} catch (Exception e) {" +
+                    "   nonmatch += " + json[0].object_info[key] +
+                    "}"
+            }
+        }
+        for (var key in json[0].parameters_info) {
+            if (json[0].parameters_info.hasOwnProperty(key) && key != '__sz') {
+                scriptString +=
+                    "try {" +
+                    "   nonmatch += (int) Math.abs(doc['parameters_info." + key + "'][0] - " + json[0].parameters_info[key] + ");" +
+                    "   additional -= doc['parameters_info." + key + "'][0];" +
+                    "} catch (Exception e) {" +
+                    "   nonmatch += " + json[0].parameters_info[key] +
+                    "}"
+            }
+        }
+        for (var key in json[0].result_info) {
+            if (json[0].result_info.hasOwnProperty(key) && key != '__sz') {
+                scriptString +=
+                    "try {" +
+                    "   nonmatch += (int) Math.abs(doc['parameters_info." + key + "'][0] - " + json[0].result_info[key] + ");" +
+                    "   additional -= doc['parameters_info." + key + "'][0];" +
+                    "} catch (Exception e) {" +
+                    "   nonmatch += " + json[0].result_info[key] +
+                    "}"
+            }
+        }
+        scriptString += "return 1 / Math.log(nonmatch + additional + 2);"
+
+        console.log(scriptString)
+
+        client.search({
+            index: 'gosearchindex',
+            type: 'function',
+            body: {
+                query: {
+                    function_score: {
+                        query: {
+                            bool: {
+                                should: [{
+                                //    terms: {"object": (json[0].object !== undefined ? json[0].object[0] : []), boost: 1}
+                                //}, {
+                                //    match: {"name": {query: (json[0].name !== undefined ? json[0].name : []), boost: 5}}
+                                //}, {
+                                    terms: {"name_parts": (json[0].name_parts !== undefined ? json[0].name_parts : []), boost: 5}
+                                //}, {
+                                //    terms: {"parameters": (json[0].parameters !== undefined ? json[0].parameters[0] : []), boost: 2}
+                                //}, {
+                                //    terms: {"result": (json[0].result !== undefined ? json[0].result[0] : []), boost: 2}
+                                }]
+                            }
+                        },
+                        functions: [
+                            {
+                                script_score: {
+                                    script: {
+                                        inline: scriptString
+                                    }
+                                }
+                            },
+                            {
+                                field_value_factor: {
+                                    field: "votes",
+                                    modifier: "log1p"
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        },function (error, response,status) {
+            if (error){
+                console.log("search error: "+error)
+            }
+            else {
+                console.log("--- Response ---");
+                console.log(response);
+                console.log("--- Hits ---");
+                response.hits.hits.forEach(function(hit){
+                    console.log(hit);
+                })
+            }
+        });
+    })
     const results = context.results
         .filter(r => (r.name || '').indexOf(query) != -1);
     res.render('index', {
